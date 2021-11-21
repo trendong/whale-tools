b85868af971186e350aba0ca1b9f8a3c62275613
trendong <13716892619@163.com>
2021-11-21 21:29:33 +0800

    feat（whale-framework）：mybatis生成代码

---
+++ whale-framework/src/main/java/com/whale/framework/repository/mapper/StudentMapper.java
@@ -0,0 +1,30 @@
+package com.whale.framework.repository.mapper;
+
+import com.whale.framework.repository.model.Student;
+import com.whale.framework.repository.model.StudentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface StudentMapper {
+    long countByExample(StudentExample example);
+
+    int deleteByExample(StudentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    List<Student> selectByExample(StudentExample example);
+
+    Student selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
